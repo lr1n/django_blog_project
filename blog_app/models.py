6f749ba76435efd09cdd136944b89040eaa70fb4
lr1n <sko0hlol@gmail.com>
2021-06-03 00:02:44 +0200

    Create models

--- blog_app/models.py
+++ blog_app/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from PIL import Image
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, verbose_name='URL', unique=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Title'
+        verbose_name_plural = 'Titles'
+        ordering = ['title']
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=80)
+    slug = models.SlugField(max_length=80, verbose_name='URL', unique=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Tag'
+        verbose_name_plural = 'Tags'
+        ordering = ['title']
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, verbose_name='URL', unique=True)
+    author = models.CharField(max_length=100)
+    content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    photo = models.ImageField(
+        upload_to='photos/%Y/%m/%d/',
+        blank=True,
+    )
+    views = models.IntegerField(default=0, verbose_name='Number of views')
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.PROTECT,
+        related_name='posts',
+    )
+    tags = models.ManyToManyField(Tag, blank=True, related_name='posts',)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-created_at']
