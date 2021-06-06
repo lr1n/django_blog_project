859a0113fef3090768f4b2870296a812dc199f76
lr1n <sko0hlol@gmail.com>
2021-06-07 01:28:25 +0200

    Add highlighting of the current category

--- blog_app/views.py
+++ blog_app/views.py
@@ -5,5 +5,5 @@ def index(request):
     return render(request, 'blog_app/index.html')
 
 
-def get_category(request):
+def get_category(request, slug):
     return render(request, 'blog_app/category.html')
