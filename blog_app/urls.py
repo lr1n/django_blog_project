90b4502e0a0fc12607c83bf3cd84dbcaccc0ccca
lr1n <sko0hlol@gmail.com>
2021-06-07 01:28:25 +0200

    Add highlighting of the current category in the menu

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -5,5 +5,5 @@ from .views import index, get_category
 
 urlpatterns = [
     path('', index, name='home'),
-    path('category/<str:slug>', get_category, name='category')
+    path('category/<str:slug>', get_category, name='category'),
 ]
