ad4437e18cc812741726ac9a39a895fc3e48483c
lr1n <sko0hlol@gmail.com>
2021-06-06 16:22:32 +0200

    Add custom template tags and template caching

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import *
+from .views import index, get_category
 
 
 urlpatterns = [
     path('', index, name='home'),
+    path('category/<str:slug>', get_category, name='category')
 ]
