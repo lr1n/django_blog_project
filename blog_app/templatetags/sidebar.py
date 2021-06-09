ea5f3645e59cefc716166c8d6c0df7d42342610b
lr1n <sko0hlol@gmail.com>
2021-06-09 12:21:12 +0200

    Add display of tags

--- blog_app/templatetags/sidebar.py
+++ blog_app/templatetags/sidebar.py
@@ -8,3 +8,9 @@ register = template.Library()
 def get_popular(cnt=3):
     posts = Post.objects.order_by('-views')[:cnt]
     return {'posts': posts}
+
+
+@register.inclusion_tag('blog_app/tags_tpl.html')
+def get_tags(cnt=20):
+    tags = Tag.objects.all()
+    return {'tags': tags}
